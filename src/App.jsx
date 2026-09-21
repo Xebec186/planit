e554517490dd9519851817abf4e91b35151e6083
Glenn Rogers <glennrogers186@gmail.com>
2026-09-21 23:10:02 +0000

    configure routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,18 @@
+import AddEventPage from "./pages/AddEventPage";
+import EventsPage from "./pages/EventsPage";
 import Homepage from "./pages/Homepage";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <div>
-      <Homepage />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Homepage />} />
+          <Route path="/events" element={<EventsPage />} />
+          <Route path="/events/add" element={<AddEventPage />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
