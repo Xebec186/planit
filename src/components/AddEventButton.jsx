f0638fc0334fee760cdb35b795892cd8343cab71
Glenn Rogers <glennrogers186@gmail.com>
2026-09-23 05:26:32 +0000

    fix shrinking in add event button

--- src/components/AddEventButton.jsx
+++ src/components/AddEventButton.jsx
@@ -5,7 +5,7 @@ function AddEventButton() {
   return (
     <Link
       to="/events/add"
-      className="flex w-fit cursor-pointer items-center gap-2 rounded-lg bg-planit-primary px-4 py-2.5 text-sm font-semibold text-white transition-all duration-200 hover:bg-planit-primary-dark hover:shadow-sm"
+      className="flex w-fit cursor-pointer items-center gap-2 rounded-lg bg-planit-primary px-4 py-2.5 text-sm font-semibold text-white transition-all duration-200 hover:bg-planit-primary-dark hover:shadow-sm shrink-0"
     >
       <IoMdAdd size={20} />
       <span>Add Event</span>
